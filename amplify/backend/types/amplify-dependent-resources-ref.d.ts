76f6eb3ce9bfc5d304573613a2e5824bbdefcf0b
Pae <songju19990426@gmail.com>
2022-04-24 23:47:37 +0900

    init model

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "testOneToMany": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
